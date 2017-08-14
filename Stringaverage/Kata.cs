c157055d724f6bb73ef1ea0246e67098490ed885
dunk <k7173968@gmail.com>
2017-08-14 10:19:57 +0800

    StringAverage

---
+++ Stringaverage/Kata.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Stringaverage
+{
+        class Kata
+    {
+        public static string AverageString(string str)
+    {
+    if (str != "")
+    {
+	 char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+	 //string text = "one two three four";
+	 string[] words = str.Split(delimiterChars);
+	 double count = 0;
+	 double f =0;
+	 string num = "";
+	 foreach(string s in words)
+	 {
+         if (s == "zero")
+         {
+             count += 0;
+         }
+		 else if(s=="one")
+		 {
+			 count+=1;
+		 }
+		 else if(s=="two")
+		 {
+			 count+=2;
+		 }
+		 else if(s=="three")
+		 {
+			 count+=3;
+		 }
+		 else if(s=="four")
+		 {
+			 count+=4;
+		 }
+		 else if(s=="five")
+		 {
+			 count+=5;
+		 }
+		 else if(s=="six")
+		 {
+			 count+=6;
+		 }
+		 else if(s=="seven")
+		 {
+			 count+=7;
+		 }
+		 else if(s=="eight")
+		 {
+			 count+=8;
+		 }
+         else if (s == "nine")
+         {
+             count += 9;
+         }
+         else 
+         {
+             return "n/a";
+         }
+		 
+	 }
+		f = Math.Floor(count/words.Length);
+		switch((int)f)
+		{
+            case 0:
+                num = "zero";
+                break;	
+			case 1:
+				num="one";
+				break;				
+			case 2:
+				num="two";
+				break;
+			case 3:
+				num="three";
+				break;
+			case 4:
+				num="four";
+				break;
+			case 5:
+				num="five";
+				break;
+			case 6:
+				num="six";
+				break;
+			case 7:
+				num="seven";
+				break;
+			case 8:
+				num="eight";
+				break;
+			case 9:
+				num="nine";
+				break;
+			default:
+                num = "n/a";
+				break;
+		}
+			return num;
+	 }
+	 else
+	 {
+         return "n/a";
+	 }
+}
+    }
+}
+
+
+
+/*
+for(int i=0;i<chars.Length;i++)
+		{
+			//count +=i;
+			switch(chars[i])
+			{
+				case 'one':
+				{
+					count +=1;
+				}
+				case 'two':
+				{
+					count +=2;
+				}
+				case 'three':
+				{
+					count +=3;
+				}
+				case 'four':
+				{
+					count +=4;
+				}
+				case 'five':
+				{
+					count +=5;
+				}
+				case 'six':
+				{
+					count +=6;
+				}
+				case 'seven':
+				{
+					count +=7;
+				}
+				case 'eight':
+				{
+					count +=8;
+				}
+				case 'nine':
+				{
+					count +=9;
+				}
+				default:
+				{
+					count +=0; // I don't sure
+				}
+			}
+		}*/
---
+++ Stringaverage/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stringaverage
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var a = Kata.AverageString("zero nine five two");
+
+           /* Assert.AreEqual("four", Kata.AverageString("zero nine five two"));
+            Assert.AreEqual("three", Kata.AverageString("four six two three"));
+            Assert.AreEqual("three", Kata.AverageString("one two three four five"));
+            Assert.AreEqual("four", Kata.AverageString("five four"));
+            Assert.AreEqual("zero", Kata.AverageString("zero zero zero zero zero"));
+            Assert.AreEqual("two", Kata.AverageString("one one eight one"));
+            Assert.AreEqual("n/a", Kata.AverageString(""));*/
+            Assert.AreEqual("n/a", Kata.AverageString("sss four"));
+        }
+    }
+}
